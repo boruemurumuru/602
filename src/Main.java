b9f79d21e8c1a1facbac36e3921cb56d92a0bf0c
한보름 <qhfma1357@naver.com>
2025-06-02 11:26:16 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         int a = 10, b = 3;
 
-        ++a;
+        a += 5; // a = a + 5;
+
         System.out.println(a);
 
-        --b;
-        System.out.println(b);
+        b -= ;
+        b *= ;
+        b /= ;
+        b %= ;
     }
 }
